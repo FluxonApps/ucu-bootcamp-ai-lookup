a8f3564397cea3ab120126453f8657ed27cfaa55
Pavlo Ruzhylo <ruzhylo.pn@ucu.edu.ua>
2025-07-14 14:09:35 +0300

    make scraper component work

---
+++ src/scripts/Scraper.tsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+type ScraperProps = {
+  api_key: string;
+  url: string;
+  country: string;
+  product?: boolean;
+};
+
+export default function Scraper({ api_key, url, country, product }: ScraperProps) {
+  const baseUrl = 'https://api.scrapingdog.com/google_lens';
+  const params: any = {
+    api_key: api_key,
+    url: url,
+    country: country,
+    product_results: product ? 'true' : 'false',
+  };
+  axios
+    .get(baseUrl, { params })
+    .then((response: any) => {
+      if (response.status === 200) {
+        console.log(response.data);
+      } else {
+        console.log('Request failed with status code: ' + response.status);
+      }
+    })
+    .catch((error: any) => {
+      console.error('Error making the request: ' + error.message);
+    });
+}
