9e375e6d6af596f690fdd558a9e5694f615d5b3a
Ivan Maksymchuk <ivan.maksymchuk4@gmail.com>
2025-07-14 14:38:13 +0300

    Added AI parser to get price from one link and also switching from .tsx to .ts

--- src/scripts/Scraper.ts
+++ src/scripts/Scraper.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import OpenAI from "openai";
 
 type ScraperProps = {
   api_key: string;
@@ -7,19 +8,21 @@ type ScraperProps = {
   product?: boolean;
 };
 
+const client = new OpenAI();
+
 export default function Scraper({ api_key, url, country, product }: ScraperProps) {
   const baseUrl = 'https://api.scrapingdog.com/google_lens';
   const params: any = {
-    api_key,
-    url,
-    country,
+    api_key: api_key,
+    url: url,
+    country: country,
     product_results: product ? 'true' : 'false',
   };
   axios
     .get(baseUrl, { params })
     .then((response: any) => {
       if (response.status === 200) {
-        console.log(response.data);
+        return response.data;
       } else {
         console.log('Request failed with status code: ' + response.status);
       }
@@ -27,4 +30,12 @@ export default function Scraper({ api_key, url, country, product }: ScraperProps
     .catch((error: any) => {
       console.error('Error making the request: ' + error.message);
     });
+}
+
+export async function PriceGetter(link:string) {
+  const response = await client.responses.create({
+    model:"gpt-4.1",
+    input:`Find and print as single number price of the product from this page ${link}`
+  })
+  console.log(response)
 }
