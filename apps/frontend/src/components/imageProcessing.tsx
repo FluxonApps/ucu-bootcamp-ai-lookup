09d8783f2702ac477d7efdc1f8b5d035855916ed
Ivan Maksymchuk <ivan.maksymchuk4@gmail.com>
2025-07-16 15:21:26 +0300

    added ImageProcessing component

---
+++ apps/frontend/src/components/imageProcessing.tsx
@@ -0,0 +1,40 @@
+
+import ImageUpload from './ImageUpload.tsx'
+import { ProfileButton } from './ProfileButton.tsx';
+import { useState } from 'react'
+
+type ImageProcessingProps = {
+  userName: string | null;
+};
+
+export default function ImageProcessing ({userName}:ImageProcessingProps) {
+  const [imageUrl, setImageUrl] = useState<string|null>(null);
+  const [country, setCountry ] = useState<string|null>(null);
+
+  const handleSearch = (data: { country: string; imageUrl: string }) => {
+    setCountry(data.country);
+    setImageUrl(data.imageUrl);
+  };
+
+  return (
+    <>
+      {imageUrl && country && (
+        <div className="fixed inset-0 flex flex-col justify-center items-center">
+          <h1 className="p-1 text-4xl font-bold bg-gradient-to-r from-[#334A40] to-[#C0D55B] bg-clip-text text-transparent">
+            Processing...
+          </h1>
+        </div>
+      )}
+
+      {!imageUrl || !country ? (
+        <div className="fixed inset-0 flex flex-col justify-center items-center">
+          <ProfileButton />
+          <h1 className="p-1 text-4xl font-bold bg-gradient-to-r from-[#334A40] to-[#C0D55B] bg-clip-text text-transparent">
+            Hello, {userName}!
+          </h1>
+          <ImageUpload onSearch={handleSearch} />
+        </div>
+      ) : null}
+    </>
+  );
+};
--- apps/frontend/src/pages/SearchPage.tsx
+++ apps/frontend/src/pages/SearchPage.tsx
@@ -1,16 +1,12 @@
 import { getAuth } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate } from 'react-router';
-import ImageUpload from '../components/ImageUpload.tsx'
-import { ProfileButton } from '../components/ProfileButton.tsx';
-import { useState } from 'react'
+import ImageProcessing from '../components/imageProcessing.tsx';
 
 const auth = getAuth();
 
 const SearchPage = () => {
   const [user, userLoading] = useAuthState(auth);
-  const [imageUrl, setImageUrl] = useState<string|null>(null);
-  const [country, setCountry ] = useState<string|null>(null);
 
   // Do not show page content until auth state is fetched.
   if (userLoading) {
@@ -22,31 +18,8 @@ const SearchPage = () => {
     return <Navigate to="/auth" replace />;
   }
 
-  const handleSearch = (data: { country: string; imageUrl: string }) => {
-    setCountry(data.country);
-    setImageUrl(data.imageUrl);
-  };
-
   return (
-    <>
-      {imageUrl && country && (
-        <div className="fixed inset-0 flex flex-col justify-center items-center">
-          <h1 className="p-1 text-4xl font-bold bg-gradient-to-r from-[#334A40] to-[#C0D55B] bg-clip-text text-transparent">
-            Processing...
-          </h1>
-        </div>
-      )}
-
-      {!imageUrl || !country ? (
-        <div className="fixed inset-0 flex flex-col justify-center items-center">
-          <ProfileButton />
-          <h1 className="p-1 text-4xl font-bold bg-gradient-to-r from-[#334A40] to-[#C0D55B] bg-clip-text text-transparent">
-            Hello, {user.displayName}!
-          </h1>
-          <ImageUpload onSearch={handleSearch} />
-        </div>
-      ) : null}
-    </>
+    <ImageProcessing userName={user.displayName} />
   );
 };
 
