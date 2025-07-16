cb788e716e25326348c78711c18f7b26c2c978ad
Ivan Maksymchuk <ivan.maksymchuk4@gmail.com>
2025-07-16 14:45:54 +0300

    Added cosmetics for ImageUpload.tsx (changed fomat of adding file, button, preview and so on) Also made Search Page interact with loaded image

--- apps/frontend/src/components/ImageUpload.tsx
+++ apps/frontend/src/components/ImageUpload.tsx
@@ -1,8 +1,18 @@
-import { useState, useEffect } from 'react';
+import React,{ useRef, useState, useEffect } from 'react';
+import Select from 'react-select';
 
-const ImageUpload = () => {
+import { countryOptions } from '../countries_list';
+
+type ImageUploadProps = {
+  onSearch: (data: { country: string; imageUrl: string }) => void;
+};
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ onSearch }) => {
+    const [selectedCountry, setSelectedCountry] = useState({value: "ua", label: "Ukraine" });
+
     const [selectedFile, setSelectedFile] = useState()
     const [preview, setPreview] = useState()
+    const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (!selectedFile) {
@@ -25,11 +35,69 @@ const ImageUpload = () => {
         setSelectedFile(e.target.files[0])
     }
 
+    // Send data back to parent
+    const handleSearchClick = () => {
+        if (selectedCountry && preview) {
+        onSearch({
+            country: selectedCountry.value,
+            imageUrl: preview,
+        });
+        }
+    };
+
     return (
-        <div>
-            <input type='image/*' onChange={onSelectFile}/>
-            {selectedFile && <img src={preview}/>}
+        <div className="mt-4 flex flex-row items-center gap-4">
+        {preview && (
+        <div className="flex w-full max-w-4xl border border-gray-300 rounded-lg p-4 relative">
+            {/* Left: Image */}
+            <img
+                src={preview}
+                alt="Preview"
+                className="w-48 h-48 object-cover rounded border border-gray-300 shadow"
+            />
+
+            {/* Right: Input + Button */}
+            <div className="flex-1 flex flex-col justify-center pl-6 relative">
+                <Select
+                options={countryOptions}
+                value={selectedCountry}
+                onChange={(option) => setSelectedCountry(option)}
+                placeholder="Select a country..."
+                isClearable
+                className="w-full"
+                classNamePrefix="react-select"
+                />
+
+                {/* Search Button: bottom-right of THIS container */}
+                <button
+                onClick={handleSearchClick}
+                className="absolute bottom-0 right-0 mb-2 mr-2 px-4 py-2 bg-gradient-to-r from-green-600 to-green-400 text-black rounded hover:from-green-700 hover:to-green-500  transition"
+                >
+                Search
+                </button>
+            </div>
         </div>
+        )}
+
+        {!preview && (
+            <>
+                <button
+                    className=" px-6 py-2 bg-[#D9D9D9] text-black font-semibold rounded-lg shadow-md hover:bg-[#c0c0c0] transition"
+                    onClick={() => inputRef.current?.click()}
+                >
+                    Upload Image
+                </button>
+
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={onSelectFile}
+                    ref={inputRef}
+                    className="hidden"
+                />
+            </>
+        )}
+    </div>
     )
 }
 
--- apps/frontend/src/pages/SearchPage.tsx
+++ apps/frontend/src/pages/SearchPage.tsx
@@ -3,11 +3,14 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate } from 'react-router';
 import ImageUpload from '../components/ImageUpload.tsx'
 import { ProfileButton } from '../components/ProfileButton.tsx';
+import { useState } from 'react'
 
 const auth = getAuth();
 
 const SearchPage = () => {
   const [user, userLoading] = useAuthState(auth);
+  const [imageUrl, setImageUrl] = useState<string|null>(null);
+  const [country, setCountry ] = useState<string|null>(null);
 
   // Do not show page content until auth state is fetched.
   if (userLoading) {
@@ -19,131 +22,31 @@ const SearchPage = () => {
     return <Navigate to="/auth" replace />;
   }
 
+  const handleSearch = (data: { country: string; imageUrl: string }) => {
+    setCountry(data.country);
+    setImageUrl(data.imageUrl);
+  };
+
   return (
-    <div className="justify-center">
-      <ProfileButton></ProfileButton>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <p className='flex w-screen'>1</p>
-      <ImageUpload></ImageUpload>
-    </div>
+    <>
+      {imageUrl && country && (
+        <div className="fixed inset-0 flex flex-col justify-center items-center">
+          <h1 className="p-1 text-4xl font-bold bg-gradient-to-r from-[#334A40] to-[#C0D55B] bg-clip-text text-transparent">
+            Processing...
+          </h1>
+        </div>
+      )}
+
+      {!imageUrl || !country ? (
+        <div className="fixed inset-0 flex flex-col justify-center items-center">
+          <ProfileButton />
+          <h1 className="p-1 text-4xl font-bold bg-gradient-to-r from-[#334A40] to-[#C0D55B] bg-clip-text text-transparent">
+            Hello, {user.displayName}!
+          </h1>
+          <ImageUpload onSearch={handleSearch} />
+        </div>
+      ) : null}
+    </>
   );
 };
 
