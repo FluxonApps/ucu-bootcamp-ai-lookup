f6cb718f7627ec182b079830e31a7c63315e6578
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-07-17 13:22:59 +0300

    Minor fixes

--- apps/frontend/src/components/HistoryQuery.tsx
+++ apps/frontend/src/components/HistoryQuery.tsx
@@ -3,19 +3,18 @@ import { useDocument } from 'react-firebase-hooks/firestore';
 import { db } from '../firebase.config.ts';
 
 export type HistoryQueryType = {
-    queryId: string;
+	queryId: string;
 };
 
-
 export function HistoryQuery({ queryId }: HistoryQueryType) {
-  const queryDoc = doc(db, "queries", queryId);
-  const [queryData] = useDocument(queryDoc);
+	const queryDoc = doc(db, "queries", queryId);
+	const [queryData] = useDocument(queryDoc);
 
-  return (
-    <div className={"h-screen grow duration-300 w-[100%]"}>
-      <div>
-        History query data page {queryData && queryData.data()?.timestamp}
-      </div>
-    </div>
-  );
+	return (
+		<div className={"h-screen grow duration-300 w-[100%]"}>
+			<div>
+				History query data page {queryData && queryData.data()?.timestamp}
+			</div>
+		</div>
+	);
 }
