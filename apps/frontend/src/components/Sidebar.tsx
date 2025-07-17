25db38e8f179c7e725b3d684a65257df80c997c4
Pavlo Ruzhylo <ruzhylo.pn@ucu.edu.ua>
2025-07-17 10:41:14 +0300

    fix imports

--- apps/frontend/src/components/Sidebar.tsx
+++ apps/frontend/src/components/Sidebar.tsx
@@ -2,52 +2,66 @@ import { getAuth } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate } from 'react-router';
 import { doc, getDoc } from 'firebase/firestore';
-import { useState, useEffect } from 'react'
-import { db } from '../../firebase.config.ts';
+import { useState, useEffect } from 'react';
+import { db } from '../firebase.config.ts';
 
 const auth = getAuth();
 
 export function Sidebar({ isOpened, activePage, setActivePage }) {
-    const getHistoryData = async () => {
-        const dbUser = doc(db, 'users', user?.uid);
-        const dbUserData = await getDoc(dbUser);
-        return dbUserData.data()?.history;
-    }
+  const getHistoryData = async () => {
+    const dbUser = doc(db, 'users', user?.uid);
+    const dbUserData = await getDoc(dbUser);
+    return dbUserData.data()?.history;
+  };
 
-    const [user, userLoading] = useAuthState(auth);
-    const [historyData, setHistoryData] = useState({});
+  const [user, userLoading] = useAuthState(auth);
+  const [historyData, setHistoryData] = useState({});
 
-    useEffect(() => {
-        getHistoryData().then(data => {
-            setHistoryData(data ? data : {});
-        });
-    }, [activePage]);
+  useEffect(() => {
+    getHistoryData().then((data) => {
+      setHistoryData(data ? data : {});
+    });
+  }, [activePage]);
 
-    // Do not show page content until auth state is fetched.
-    if (userLoading) {
-        return null;
-    }
+  // Do not show page content until auth state is fetched.
+  if (userLoading) {
+    return null;
+  }
 
-    // If user isn't signed in, redirect to auth page.
-    if (!user) {
-        return <Navigate to="/auth" replace />;
-    }
+  // If user isn't signed in, redirect to auth page.
+  if (!user) {
+    return <Navigate to="/auth" replace />;
+  }
 
-    return (
-        <div className={"h-screen bg-amber-50 duration-300 origin-left fixed l-0 overflow-x-hidden w-0 overflow-y-hidden " + (isOpened && "w-100 border-r-2 overflow-y-scroll")}>
-            <div className="min-w-50 flex flex-col-reverse">
-                {Object.entries(historyData).sort().map((query, i) => {
-                    return (
-                        <div key={i} onClick={() => {
-                            setActivePage(query[0]);
-                        }} className={"flex justify-between items-center min-w-10 border-2 rounded-lg p-2 hover:scale-102 duration-300 cursor-pointer m-2 " + (query[0] == activePage && "scale-102")}>
-                            <img src={query[1].image} className="w-10 h-10"></img>
-                            <p className={"duration-300 " + (query[0] == activePage && "text-xl")}>{query[1].date}</p>
-                        </div>
-                    )
-                })}
-                <h1 className="text-3xl flex items-center justify-center h-22 ml-1">History</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={
+        'h-screen bg-amber-50 duration-300 origin-left fixed l-0 overflow-x-hidden w-0 overflow-y-hidden ' +
+        (isOpened && 'w-100 border-r-2 overflow-y-scroll')
+      }
+    >
+      <div className="min-w-50 flex flex-col-reverse">
+        {Object.entries(historyData)
+          .sort()
+          .map((query, i) => {
+            return (
+              <div
+                key={i}
+                onClick={() => {
+                  setActivePage(query[0]);
+                }}
+                className={
+                  'flex justify-between items-center min-w-10 border-2 rounded-lg p-2 hover:scale-102 duration-300 cursor-pointer m-2 ' +
+                  (query[0] == activePage && 'scale-102')
+                }
+              >
+                <img src={query[1].image} className="w-10 h-10"></img>
+                <p className={'duration-300 ' + (query[0] == activePage && 'text-xl')}>{query[1].date}</p>
+              </div>
+            );
+          })}
+        <h1 className="text-3xl flex items-center justify-center h-22 ml-1">History</h1>
+      </div>
+    </div>
+  );
 }
--- apps/frontend/src/pages/SearchPage.tsx
+++ apps/frontend/src/pages/SearchPage.tsx
@@ -9,7 +9,7 @@ import { ActiveQuery } from '../components/ActiveQuery.tsx';
 import { HistoryQuery } from '../components/HistoryQuery.tsx';
 import { useState } from 'react';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { db } from '../../firebase.config.ts';
+import { db } from '../firebase.config.ts';
 import testPicture from '../assets/default-user.jpg';
 
 const auth = getAuth();
@@ -29,28 +29,35 @@ const SearchPage = () => {
     return <Navigate to="/auth" replace />;
   }
 
-  const processQuery = async queryData => {
+  const processQuery = async (queryData) => {
     // here queryData must be processed and links to needed images must be saved
     const dbUser = doc(db, 'users', user?.uid);
     const dbUserData = await getDoc(dbUser);
     const newId = new Date().getTime();
     let historyObj = dbUserData.data()?.history;
     historyObj[newId] = {
-      date: "Query " + (Object.entries(historyObj).length + 1),
-      image: testPicture
-    }
+      date: 'Query ' + (Object.entries(historyObj).length + 1),
+      image: testPicture,
+    };
     await updateDoc(dbUser, { history: historyObj });
     setActivePage(newId);
-  }
+  };
 
   return (
     <div className="justify-center">
       <ProfileButton></ProfileButton>
-      <HistoryButton callback={isOpened => setSidebarOpened(isOpened)}></HistoryButton>
-      <Sidebar isOpened={isSidebarOpened} history={history} activePage={activePage} setActivePage={setActivePage}></Sidebar>
-      {activePage == -1
-      ? <ActiveQuery processQuery={processQuery}></ActiveQuery>
-      : <HistoryQuery historyPage={activePage} newQueryCallback={() => setActivePage(-1)}></HistoryQuery>}
+      <HistoryButton callback={(isOpened) => setSidebarOpened(isOpened)}></HistoryButton>
+      <Sidebar
+        isOpened={isSidebarOpened}
+        history={history}
+        activePage={activePage}
+        setActivePage={setActivePage}
+      ></Sidebar>
+      {activePage == -1 ? (
+        <ActiveQuery processQuery={processQuery}></ActiveQuery>
+      ) : (
+        <HistoryQuery historyPage={activePage} newQueryCallback={() => setActivePage(-1)}></HistoryQuery>
+      )}
       <ImageUpload></ImageUpload>
     </div>
   );
